9ef7bfcd26eef9383e3c5c53d2f5205b245fca71
张海 <zhanghai@woda.ink>
2026-03-28 18:13:51 +0800

    -优雅关停

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/Go5303/gin_demo/config"
 	"github.com/Go5303/gin_demo/internal/model"
@@ -62,18 +65,33 @@ func main() {
 	// Setup routes
 	router.Setup(r, cfg)
 
-	// Graceful shutdown
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
+	// Create HTTP server
 	addr := fmt.Sprintf(":%d", cfg.Server.Port)
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	// Start server in goroutine
 	go func() {
 		logger.L.Infof("server starting on %s", addr)
-		if err := r.Run(addr); err != nil {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			logger.L.Fatalf("server failed to start: %v", err)
 		}
 	}()
 
+	// Wait for interrupt signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	logger.L.Info("server shutting down...")
+
+	// Give outstanding requests 10 seconds to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.L.Errorf("server forced to shutdown: %v", err)
+	}
+
+	logger.L.Info("server exited")
 }
